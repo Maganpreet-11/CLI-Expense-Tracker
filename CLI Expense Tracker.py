6d4bb06eb5e4312988961d037b0e52e523b813c7
Maganpreet Singh <as1783268@gmail.com>
2026-04-16 13:43:17 +0530

    Implement edit and filter by category features
    Added functionality to edit and filter expenses by category.

--- CLI Expense Tracker.py
+++ CLI Expense Tracker.py
@@ -71,14 +71,73 @@ def delete_expense():
     else:
         print("❌ Invalid index!\n")
 
+def edit_expense():
+    if not expenses:
+        print("⚠️ No expenses to edit.\n")
+        return
+    
+    view_expenses()
+
+    try:
+        index = int(input("Enter expense number to edit: ")) - 1
+    except ValueError:
+        print("❌ Please enter a valid number.\n")
+        return
+    
+    # Check if index is valid BEFORE asking for new data
+    if 0 <= index < len(expenses):
+        try:
+            amount = float(input("Enter new amount: ₹"))
+            if amount <= 0:
+                print("❌ Amount must be greater than 0.\n")
+                return
+        except ValueError:
+            print("❌ Invalid amount! Please enter a number.\n")
+            return
+
+        category = input("Enter new category: ").strip()
+        if not category:
+            print("❌ Category cannot be empty.\n")
+            return
+        
+        expense = {
+            "amount" : amount,
+            "category" : category
+        }
+        
+        expenses[index] = expense
+        save_expenses()
+        print("✅ Expense updated successfully!\n")
+    else:
+        print("❌ Invalid index!\n")
+
+def filter_by_category():
+    if not expenses:
+        print("⚠️ No expenses.\n")
+        return
+    
+    category = input("Enter the category to filter: ").strip().lower()
+    found = False
+    
+    print(f"\n🔍 Results for '{category}':")
+    for item in expenses:
+        # Compare the lowercase version of the saved category
+        if item["category"].lower() == category:
+            print(f"- ₹{item['amount']}")
+            found = True
+            
+    if not found:
+        print(f"❌ No expenses found in the '{category}' category.")
+    print() # Add an empty line for clean spacing
 
 def show_menu():
     print("===== Expense Tracker =====")
     print("1. Add Expense")
     print("2. View Expenses")
     print("3. Delete Expense")
-    print("4. Exit")
-
+    print("4. Edit Expense")
+    print("5. View by Category")
+    print("6. Exit")
 
 def main():
     load_expenses()
@@ -93,11 +152,14 @@ def main():
         elif choice == "3":
             delete_expense()
         elif choice == "4":
-            print("👋 Exiting... Goodbye!")
+            edit_expense()
+        elif choice == "5":
+            filter_by_category()
+        elif choice == '6':
+            print("Goodbye! 👋")
             break
         else:
             print("❌ Invalid choice! Try again.\n")
-
 
 if __name__ == "__main__":
     main()
